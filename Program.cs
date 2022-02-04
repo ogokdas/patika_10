8b95ce2db1326fd899bdd26e5913a6e8ab596035
ogokdas <ogokdas@yandex.com>
2022-02-05 00:33:29 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace patika_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayiDizisi = new int[20];
+            int toplam = 0;
+            for (int i = 0; i < sayiDizisi.Length; i++)
+            {
+                Console.Write("Lütfen {0}. sayıyı girin:", i + 1);
+                sayiDizisi[i] = Convert.ToInt32(Console.ReadLine());
+                toplam += sayiDizisi[i];
+            }
+            double ortalama = toplam / 20;
+            Console.WriteLine("Sayıların ortalaması: " + ortalama);
+            Console.ReadLine();
+        }
+    }
+}
